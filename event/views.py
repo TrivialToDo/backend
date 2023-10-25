733231cab5ace27ce53792276dbc070d161820d3
Xial <450326581@qq.com>
2023-10-25 23:41:29 +0800

    :art: style: fix code style

--- event/views.py
+++ event/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-from django.views.decorators.http import require_http_methods
-from .models import Event
 from django.http import JsonResponse
-from datetime import time, datetime, date
-import hashlib
+from django.views.decorators.http import require_http_methods
+
+from .models import Event
 
 
 # Create your views here.
