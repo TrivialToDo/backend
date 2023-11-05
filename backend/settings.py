b0575568b955546e94c2bf5b9ee9a779074f1920
Xial <450326581@qq.com>
2023-11-05 21:50:25 +0800

    :rocket: chore: add deploy files

--- backend/settings.py
+++ backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b3h0)hqduvaza1_=pk00u2nu6@s$%sbettaey=4po+ohl0x%*j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.getenv('DEPLOY') else True
 
 ALLOWED_HOSTS = []
 
