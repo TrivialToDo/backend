11caf55eceda3d5c18a116247db3a4315590b5ed
kaiming <chatkming@gmai.com>
2023-12-09 12:16:42 +0800

    :sparkles: feat: add audio translations api

--- wechat/tests.py
+++ wechat/tests.py
@@ -1,1 +1,21 @@
-# Create your tests here.
+from django.test import TestCase
+import json
+import base64
+
+class EventTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_audio(self):
+        with open('./test_files/audio_test.mp3', 'rb') as f:
+            audio_content = base64.b64encode(f.read()).decode('utf-8')
+        data = {
+            'id': 'test_id',
+            'name': 'test_name',
+            'date': "2023-10-26T10:56:30.000Z",
+            'type': 'audio',
+            'content': audio_content
+        }
+        resp = self.client.post('/wechat/receive_msg', data=json.dumps(data), content_type='application/json')
+        print(resp.json)
+        self.assertEqual(resp.status_code, 200)
--- wechat/views.py
+++ wechat/views.py
@@ -9,13 +9,29 @@ from agent.views import agent_main
 from user.models import User
 import asyncio
 
+import backoff
+import openai
+import os
+import base64
 
 # Create your views here.
+
+@backoff.on_exception(backoff.expo, Exception, max_time=60)
+def process_audio(audio_path: str) -> str:
+    message = openai.audio.translations.create(
+        file=open(audio_path, 'rb'),
+        model='whisper-1',
+        prompt='你需要将这段话转换成简体中文:\n',
+        response_format='text',
+        temperature=0
+    )
+    return message
 
 
 @require_http_methods(['POST'])
 def recv_msg(req):
     body = json.loads(req.body.decode('utf-8'))
+    type = body['type']
     wechat_id = body['id']
     nickname = body['name']
     msg_time = body['date']
@@ -25,7 +41,7 @@ def recv_msg(req):
     if user is None:
         user = User.objects.create(wechat_id=wechat_id, nickname=nickname)
 
-    if body['content'] == '查日程':
+    if type == 'text' and body['content'] == '查日程':
         return request_success({
             "type": "text",
             "content": user.generate_temp_token()
@@ -36,6 +52,14 @@ def recv_msg(req):
             "type": "text",
             "content": "get off!"
         })
+    
+    if type == 'audio':
+        audio_name = wechat_id
+        audio_path = f'./{audio_name}.mp3'
+        with open(audio_path, 'wb') as f:
+            f.write(base64.b64decode(body['content']))
+        body['content'] = process_audio(audio_path)
+        os.remove(audio_path)
 
     try:
         user.agent_deal = True
