c2d4a9220422f81cefd17f8b8f05574886c9d061
kaiming <chatkming@gmai.com>
2023-12-09 14:23:28 +0800

    :sparkles: feat: add modify api

--- event/urls.py
+++ event/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('new', views.new_event, name='new_event'),
+    path('modify', views.modify_event, name='modify_event'),
     path('<str:hash>', views.deal_event, name='deal_event'),
 ]
--- event/views.py
+++ event/views.py
@@ -8,12 +8,13 @@ from user.models import User
 
 # Create your views here.
 
+
 @login_check
-@require_http_methods(['GET', 'DELETE'])
+@require_http_methods(["GET", "DELETE"])
 def deal_event(request, hash: str, user: User = None):
-    if request.method == 'GET':
+    if request.method == "GET":
         return get_event(request, hash, user)
-    elif request.method == 'DELETE':
+    elif request.method == "DELETE":
         return del_event(request, hash, user)
 
 
@@ -21,9 +22,7 @@ def get_event(request, hash: str, user: User = None):
     res = Event.objects.filter(hash=hash, user=user).first()
     if res is None:
         return request_fail(404, "event not found")
-    return request_success({
-        "event": res.serialize()
-    })
+    return request_success({"event": res.serialize()})
 
 
 def del_event(request, hash: str, user: User = None):
@@ -31,21 +30,43 @@ def del_event(request, hash: str, user: User = None):
     if res is None:
         return request_fail(404, "event not found")
     res.delete()
-    return request_success({
-        "msg": "Succeed"
-    })
+    return request_success({"msg": "Succeed"})
+
 
 @login_check
-@require_http_methods(['POST'])
+@require_http_methods(["POST"])
 def new_event(request, user: User = None):
-    Event.create_event(
+    event, response = Event.create_event(
         user=user,
-        time_start=request.POST.get('timeStart'),
-        date_start=request.POST.get('dateStart'),
-        time_end=request.POST.get('timeEnd'),
-        date_end=request.POST.get('dateEnd'),
-        title=request.POST.get('title'),
-        description=request.POST.get('description'),
-        repeat=request.POST.get('repeat'),
-        reminder=request.POST.get('reminder'),
+        time_start=request.POST.get("timeStart"),
+        date_start=request.POST.get("dateStart"),
+        time_end=request.POST.get("timeEnd"),
+        date_end=request.POST.get("dateEnd"),
+        title=request.POST.get("title"),
+        description=request.POST.get("description"),
+        repeat=request.POST.get("repeat"),
+        reminder=request.POST.get("reminder"),
     )
+    if response is not None:
+        return response
+    return request_success({"hash": event.hash})
+
+
+@login_check
+@require_http_methods(["POST"])
+def modify_event(request, user: User = None):
+    event, response = Event.modify_event(
+        user=user,
+        hash=request.POST.get("hash"),
+        time_start=request.POST.get("timeStart"),
+        date_start=request.POST.get("dateStart"),
+        time_end=request.POST.get("timeEnd"),
+        date_end=request.POST.get("dateEnd"),
+        title=request.POST.get("title"),
+        description=request.POST.get("description"),
+        repeat=request.POST.get("repeat"),
+        reminder=request.POST.get("reminder"),
+    )
+    if response is not None:
+        return response
+    return request_success({"hash": event.hash})
