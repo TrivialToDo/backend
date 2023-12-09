646e5cca907da37ab07852b254b8721becb5ccda
kaiming <chatkming@gmai.com>
2023-12-09 14:25:06 +0800

    :test_tube: test: add event app failing test

--- event/tests.py
+++ event/tests.py
@@ -7,14 +7,58 @@ from .models import Event
 
 class EventTestCase(TestCase):
     def setUp(self):
-        self.e1, _ = Event.create_event({"hour": 0, "minute": 0}, "2020-01-10")
-        self.e2, _ = Event.create_event({"hour": 0, "minute": 0}, "2020-01-01", repeat="daily")
+        # resp = self.get("/token")
+        self.test_token = "Bearer " + "???"
 
-    def test_get_event(self):
-        resp = self.client.get(f'/event/{self.e1.hash}')
+    def test_create_event(self):
+        resp = self.client.post(
+            "/event/new",
+            {
+                "timeStart": {"hour": 0, "minute": 0},
+                "dateStart": "2020-01-01",
+            },
+            HTTP_AUTHORIZATION=self.test_token
+        )
         self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.json()['data']['event']['hash'], self.e1.hash)
+        self.e1_hash = resp.json()["data"]["hash"]
+
+    def test_get_event_1(self):
+        resp = self.client.get(f"/event/{self.e1_hash}")
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()["data"]["event"]["hash"], self.e1_hash)
+        self.assertEqual(resp.json()["data"]["event"]["title"], "Untitled")
+
+    def test_modify_event_1(self):
+        resp = self.client.post(
+            "/event/modify",
+            {
+                "hash": self.e1_hash,
+                "title": "test",
+                "timeStart": {"hour": 0, "minute": 0},
+                "dateStart": "2020-01-01",
+            },
+            HTTP_AUTHORIZATION=self.test_token
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()["data"]["hash"], self.e1_hash)
+        self.assertEqual(resp.json()["data"]["title"], "test")
+
+    def test_modify_event_2(self):
+        resp = self.client.post(
+            "/event/modify",
+            {
+                "hash": "123",
+                "title": "test",
+                "timeStart": {"hour": 0, "minute": 0},
+                "dateStart": "2020-01-01",
+                "repeat": "daily",
+            },
+            HTTP_AUTHORIZATION=self.test_token
+        )
+        self.assertEqual(resp.status_code, 404)
 
     def test_del_event(self):
-        resp = self.client.delete(f'/event/{self.e1.hash}')
+        resp = self.client.delete(f"/event/{self.e1_hash}")
         self.assertEqual(resp.status_code, 200)
+        resp = self.client.get(f"/event/{self.e1_hash}")
+        self.assertEqual(resp.status_code, 404)
