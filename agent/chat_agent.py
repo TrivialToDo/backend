dfe94d936f8b750527409bdc626bd25b03a30836
kaiming <chatkming@gmai.com>
2023-12-09 21:22:39 +0800

    :sparkles: feat: add tools for ChatAgent

--- agent/chat_agent.py
+++ agent/chat_agent.py
@@ -1,4 +1,6 @@
+from datetime import datetime, timedelta
 from agent.base_agent import BaseAgent
+from schedule.views import get_day_event
 from user.models import User
 from typing import Tuple
 import logging
@@ -13,11 +15,31 @@ class ChatAgent(BaseAgent):
             self.system_prompt = f.read()
         with open("agent/functions/chat_agent.json", encoding="utf-8") as f:
             self.functions = json.load(f)
-        self.available_function = {"send_message": self.send_message}
+        self.available_function = {
+            "send_message": self.send_message,
+            "send_schedule": self.send_schedule,
+            "get_web_url": self.get_web_url,
+        }
 
     def send_message(self, message: str) -> Tuple[str, bool, bool]:
         logging.info(f"🔧 {self.__str__()} Function Calling: send_message({message})")
         return message, True, True
+    
+    def send_schedule(self, date: str) -> Tuple[str, bool, bool]:
+        logging.info(f"🔧 {self.__str__()} Function Calling: send_schedule({date})")
+        events = []
+        for i in range(7):
+            _date = datetime.strptime(date, "%Y-%m-%d").date()
+            _date += timedelta(days=i)
+            r = get_day_event(_date, self.user)
+            events.extend([e for e in r])
+        #TODO
+        return date, True, False
+    
+    def get_web_url(self) -> Tuple[str, bool, bool]:
+        logging.info(f"🔧 {self.__str__()} Function Calling: get_web_url()")
+        token = self.user.generate_temp_token()
+        return f"todo.yuan.cf/login?token={token}", True, False
 
     def __str__(self) -> str:
         return "ChatAgent"
